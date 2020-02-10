d325053c625e0db10ac27599398f6071e5ce44d3
SHREYA <shreyachandra975@gmail.com>
2020-02-10 13:19:57 +0530

    my first commit

---
+++ flutter_course1/flutter_course_app1/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+    void main(){
+       runApp(App1());
+
+    }
+
+    class App1 extends StatelessWidget {
+           @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(home: Text('Hello!'),);
+  }
+
+    }
